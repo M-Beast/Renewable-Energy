79aa93eb1147b3c30453ba5786064f997a991bf7
DG950 <adham.kassem950@gmail.com>
2023-03-14 23:40:10 +0200

    Removed the comment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const Port = 3000; // server port
+const Port = 3000;
 
 // The Defined routes
 const routes = {
